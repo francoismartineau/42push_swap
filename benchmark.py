df439d053e712bc923c3ed5a091c7ab49c0b326b
François Martineau <ffrancoismmartineau@gmail.com>
2023-03-29 08:50:48 -0400

    benchmark

---
+++ benchmark.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+if (len(sys.argv) < 3 or not sys.argv[1].isdigit() or not sys.argv[2].isdigit()):
+	print("Usage: python3 benchmark.py <number of elements> <number of tests>")
+	exit(1)
+
+average = 0
+test_qty = int(sys.argv[2])
+cmd = f'./push_swap $(python3 generator.py {sys.argv[1]})'
+for _ in range(test_qty):
+	res = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode('utf-8')
+	average += len(res.split('\n'))
+print(f'Average number of instructions: {average / test_qty}')
